e60d1d26d678c13fd5f5e5461f3eb1cada1811b4
SuyogSky <shakyasuyog214@gmail.com>
2023-07-18 14:12:48 +0545

    Commit 4

--- src/ip/ip.js
+++ src/ip/ip.js
@@ -1,4 +1,4 @@
-const ip = () => "https://lost-and-found-api.onrender.com"
+const ip = () => "https://lostandfound-xv1t.onrender.com"
 // const ip = () => "192.168.255.193:5000"
 // const ip = () => "http://192.168.1.69:5000"
 
